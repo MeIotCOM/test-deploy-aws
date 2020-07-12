35f3b8b428a88c0d44091f39d57cb002701c447a
Barbossa <zicowarn@hotmail.com>
2020-07-12 09:16:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-test
- s
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello Lambda, from Github!'),
+    };
+    return response;
+};
