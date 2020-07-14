5c90c259e6285182158f4610c2082116fa3b4fcd
mrwang <zicowarn@hotmail.com>
2020-07-14 15:22:58 +0800

    测试
    测试

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Lambda, from Github, Again 4!'),
+        body: JSON.stringify('Hello Lambda, from Github, Again 3!'),
     };
     return response;
 };
