1cf5c46e92cf1a15b63cf6868bea69a80ee120fa
mrwang <zicowarn@hotmail.com>
2020-07-14 15:39:18 +0800

    cde
    dee

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Lambda, from Github, Again 5!'),
+        body: JSON.stringify('Hello Lambda, from Github, Again 4!'),
     };
     return response;
 };
